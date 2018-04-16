9edd0f1f231b2ee77937e462735c476b030a01b6
Stephanos Bacon <stephanosbacon@gmail.com>
2018-04-15 22:32:14 -0400

    tweaks

--- index.js
+++ index.js
@@ -10,46 +10,55 @@ var minioClient = new Minio.Client({
 
 console.log('here');
 
+var done = false;
+
 minioClient.makeBucket('mybucket', 'us-east-1', function(err) {
   if (err) {
     console.log('Error creating bucket.', err);
     return;
   }
   console.log('Bucket created successfully in "us-east-1".');
+
+  console.log('after bucket');
+
+  var buffer = 'Hello World';
+
+  minioClient.putObject('mybucket', 'hello-file', buffer, function(err, etag) {
+    console.log(err, etag); // err should be null
+
+    var size = 0;
+  console.log('after put');
+
+    minioClient.getObject('mybucket', 'hello-file', function(err, dataStream) {
+      if (err) {
+        console.log(err);
+        return;
+      }
+      dataStream.on('data', function(chunk) {
+        size += chunk.length;
+      });
+      
+      dataStream.on('end', function() {
+        console.log('End. Total size = ' + size);
+      });
+      
+      dataStream.on('error', function(err) {
+        console.log(err);
+      });
+
+      console.log('all good in the hood');
+
+      done = true;
+      
+    });
+  });
 });
 
-console.log('after bucket');
-
-var buffer = 'Hello World';
-
-minioClient.putObject('mybucket', 'hello-file', buffer, function(err, etag) {
-  console.log(err, etag); // err should be null
-});
-
-console.log('after put');
+while (done == false) {
+  console.log('waiting');
+}
 
 
-var size = 0;
-
-minioClient.getObject('mybucket', 'photo.jpg', function(err, dataStream) {
-  if (err) {
-    return console.log(err);
-  }
-  dataStream.on('data', function(chunk) {
-    size += chunk.length;
-  });
-  
-  dataStream.on('end', function() {
-    console.log('End. Total size = ' + size);
-  });
-  
-  dataStream.on('error', function(err) {
-    console.log(err);
-  });
-
-  return console.log('all good in the hood');
-  
-});
 
 
 
